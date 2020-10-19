7cb247a85e4c9794e8f12660fe3e750d95de2c39
Yliya Pehota <pehotaig@gmail.com>
2020-10-19 23:25:59 +0300

    init: task Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+let IntToStr = Math.abs(n).toString();
+let i = IntToStr.length-1;
+let result=" ";
+    
+    while (i >= 0) {
+          result += IntToStr[i];
+          i = i - 1;
+    }
+
+return result;
 }
